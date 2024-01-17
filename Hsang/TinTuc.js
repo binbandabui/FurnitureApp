a503fd825d15cdd0e88adeb26ea0bcdfe5458a8e
HSang2484 <156687904+myaln203@users.noreply.github.com>
2024-01-17 23:52:20 +0700

    Update TinTuc.js

--- Hsang/TinTuc.js
+++ Hsang/TinTuc.js
@@ -1,14 +1,14 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React,{useState} from 'react'
 
-const TinTuc = () => {
+const TinTuc = (props) => {
+    const [data, setData] = useState(DATA)
   return (
     <View>
 
       <View>
-
         <View>
-            <Image/>
+            <Image source={require('../asset/img/tintuc.png')}/>
         </View>
         <View>
             <Text></Text>
@@ -23,3 +23,33 @@ export default TinTuc
 
 const styles = StyleSheet.create({})
 
+ var DATA=[
+    {
+        tieude:'Your order #123456789 has been confirmed',
+        noidung:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis pretium et in arcu adipiscing nec. Turpis pretium et in arcu adipiscing nec. '
+    },
+    {
+        tieude:'Your order #123456789 has been confirmed',
+        noidung:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis pretium et in arcu adipiscing nec. Turpis pretium et in arcu adipiscing nec. '
+    },
+    {
+        tieude:'Your order #123456789 has been confirmed',
+        noidung:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis pretium et in arcu adipiscing nec. Turpis pretium et in arcu adipiscing nec. '
+    },
+    {
+        tieude:'Your order #123456789 has been confirmed',
+        noidung:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis pretium et in arcu adipiscing nec. Turpis pretium et in arcu adipiscing nec. '
+    },
+    {
+        tieude:'Your order #123456789 has been confirmed',
+        noidung:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis pretium et in arcu adipiscing nec. Turpis pretium et in arcu adipiscing nec. '
+    },
+    {
+        tieude:'Your order #123456789 has been confirmed',
+        noidung:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis pretium et in arcu adipiscing nec. Turpis pretium et in arcu adipiscing nec. '
+    },
+    {
+        tieude:'Your order #123456789 has been confirmed',
+        noidung:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis pretium et in arcu adipiscing nec. Turpis pretium et in arcu adipiscing nec. '
+    },
+ ]
