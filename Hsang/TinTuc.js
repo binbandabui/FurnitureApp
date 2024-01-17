fb1e17d98e085f4215bd0cca8a198cf74f05f19d
HSang2484 <156687904+myaln203@users.noreply.github.com>
2024-01-17 23:40:13 +0700

    TinTuc

---
+++ Hsang/TinTuc.js
@@ -0,0 +1,25 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TinTuc = () => {
+  return (
+    <View>
+
+      <View>
+
+        <View>
+            <Image/>
+        </View>
+        <View>
+            <Text></Text>
+            <Text></Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default TinTuc
+
+const styles = StyleSheet.create({})
+
