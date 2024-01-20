e7896d3c0a322e87c31fafdd57f3dfcedccf4505
binbandabui <126151527+binbandabui@users.noreply.github.com>
2024-01-20 19:29:12 +0700

    Hsang

--- App.tsx
+++ App.tsx
@@ -12,12 +12,14 @@ import Favor from './src/Favorite';
 import Product from './src/Product';
 import Home from './src/Home';
 import Main from './src/Main';
+import TinTuc from './src/TinTuc';
+import ManKhoiDau from './src/ManKhoiDau';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar translucent backgroundColor={'rgba(0,0,0,0)'} />
-      <Main />
+      <ManKhoiDau />
     </SafeAreaView>
   );
 }
