d1239ed1a4ffff55d27abff6ba3fc1086d1dd0b4
ɭê ɦùռɠ <125887825+nguoilaisaochoi@users.noreply.github.com>
2024-01-20 14:54:44 +0700

    v1.3.2

--- App.tsx
+++ App.tsx
@@ -10,12 +10,13 @@ import {
 } from 'react-native';
 import Favor from './src/Favorite';
 import Product from './src/Product';
+import Home from './src/Home';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={{flex: 1}}>
       <StatusBar translucent backgroundColor={'rgba(0,0,0,0)'} />
-      <Product />
+      <Home />
     </SafeAreaView>
   );
 }
