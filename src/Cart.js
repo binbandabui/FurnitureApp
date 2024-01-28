8fc9f874a3e6fd0020f98c39c30ffa2e6f4f0471
HSang2484 <156687904+myaln203@users.noreply.github.com>
2024-01-28 21:28:02 +0700

    a

---
+++ src/Cart.js
@@ -0,0 +1,90 @@
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+
+const Cart = () => {
+
+  const [data, setData] = useState(DATA)
+  const renderItem = ({ item }) => {
+    const { avt, ten, gia } = item;
+    return (
+      <View style={styles.cart}>
+        <View>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: item.avt }}
+        />
+        </View>
+        <View style={styles.thongtin}>
+          <View>
+            <Text style={{color:'gray', fontSize:25}}>{ten}</Text>
+            <Text style={{fontWeight:'bold', fontSize:25}}>$ + {gia}</Text>
+          </View>
+          <View>
+            <Image source={require('../asset/img/imgnav/plus.png')} />
+            <Text>01</Text>
+            <Image source={require('../asset/img/imgnav/tru.png')} />
+          </View>
+        </View>
+        <View style={styles.shape}>
+          <Image source={require('../asset/img/imgnav/Shape.png')}/>
+        </View>
+      </View>
+    );
+  }
+  return (
+    <View>
+      <FlatList />
+    </View>
+  )
+}
+
+export default Cart
+
+const styles = StyleSheet.create({
+  cart:{
+    width:'100%',
+    flexDirection:'row'
+  },
+  thongtin:{
+    justifyContent:'space-between'
+  }
+})
+
+var DATA = [
+  {
+    avt: 'https://bietmaytinh.com/wp-content/uploads/2021/06/cach-cat-anh-hinh-vuong-4.png',
+    ten: 'Alibaba',
+    gia: 20.00
+  },
+  {
+    avt: 'https://bietmaytinh.com/wp-content/uploads/2021/06/cach-cat-anh-hinh-vuong-4.png',
+    ten: 'Alibaba',
+    gia: 20.00
+  },
+  {
+    avt: 'https://bietmaytinh.com/wp-content/uploads/2021/06/cach-cat-anh-hinh-vuong-4.png',
+    ten: 'Alibaba',
+    gia: 20.00
+  },
+  {
+    avt: 'https://bietmaytinh.com/wp-content/uploads/2021/06/cach-cat-anh-hinh-vuong-4.png',
+    ten: 'Alibaba',
+    gia: 20.00
+  },
+  {
+    avt: 'https://bietmaytinh.com/wp-content/uploads/2021/06/cach-cat-anh-hinh-vuong-4.png',
+    ten: 'Alibaba',
+    gia: 10.00
+  },
+  {
+    avt: 'https://bietmaytinh.com/wp-content/uploads/2021/06/cach-cat-anh-hinh-vuong-4.png',
+    ten: 'Alibaba',
+    gia: 25.00
+  },
+  {
+    avt: 'https://bietmaytinh.com/wp-content/uploads/2021/06/cach-cat-anh-hinh-vuong-4.png',
+    ten: 'Alibaba',
+    gia: 20.00
+  },
+
+]
