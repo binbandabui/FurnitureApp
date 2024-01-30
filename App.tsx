5805fc6a6a7f08271d40318cda684da80a125f0a
HSang2484 <156687904+myaln203@users.noreply.github.com>
2024-01-30 19:37:45 +0700

    a

--- App.tsx
+++ App.tsx
@@ -14,13 +14,14 @@ import Home from './src/Home';
 import Main from './src/Main';
 import TinTuc from './src/TinTuc';
 import ManKhoiDau from './src/ManKhoiDau';
+import Cart from './src/Cart';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar translucent backgroundColor={'rgba(0,0,0,0)'} />
-      <ManKhoiDau />
-      <Text>Aaas</Text>
+      <Cart/>
+      
     </SafeAreaView>
   );
 }
