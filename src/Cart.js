505a818f91c3d3ab90acabea9a6c7af88b3e4787
HSang2484 <156687904+myaln203@users.noreply.github.com>
2024-01-30 16:06:10 +0700

    uup

--- src/Cart.js
+++ src/Cart.js
@@ -1,4 +1,4 @@
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 
 const Cart = () => {
@@ -9,15 +9,15 @@ const Cart = () => {
     return (
       <View style={styles.cart}>
         <View>
-        <Image
-          style={{ width: 100, height: 100 }}
-          source={{ uri: item.avt }}
-        />
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: item.avt }}
+          />
         </View>
         <View style={styles.thongtin}>
           <View>
-            <Text style={{color:'gray', fontSize:25}}>{ten}</Text>
-            <Text style={{fontWeight:'bold', fontSize:25}}>$ + {gia}</Text>
+            <Text style={{ color: 'gray', fontSize: 25 }}>{ten}</Text>
+            <Text style={{ fontWeight: 'bold', fontSize: 25 }}>$ + {gia}</Text>
           </View>
           <View>
             <Image source={require('../asset/img/imgnav/plus.png')} />
@@ -26,14 +26,27 @@ const Cart = () => {
           </View>
         </View>
         <View style={styles.shape}>
-          <Image source={require('../asset/img/imgnav/Shape.png')}/>
+          <Image source={require('../asset/img/imgnav/Shape.png')} />
         </View>
       </View>
     );
   }
   return (
     <View>
-      <FlatList />
+      <FlatList
+
+      />
+
+      <View>
+        <View>
+          <Text>Total: </Text>
+          <Text>9.000 </Text>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{textAlign:'center', fontSize:20}}>Check out</Text>
+        </TouchableOpacity>
+
+      </View>
     </View>
   )
 }
@@ -41,12 +54,18 @@ const Cart = () => {
 export default Cart
 
 const styles = StyleSheet.create({
-  cart:{
-    width:'100%',
-    flexDirection:'row'
+  cart: {
+    width: '100%',
+    flexDirection: 'row'
   },
-  thongtin:{
-    justifyContent:'space-between'
+  thongtin: {
+    justifyContent: 'space-between '
+  },
+
+  button:{
+    width:'100%',
+    backgroundColor:'black',
+    borderRadius:5
   }
 })
 
