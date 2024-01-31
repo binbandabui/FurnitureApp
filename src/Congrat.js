d544a9a18621974a8212b355785688a643dd36f9
binbandabui <126151527+binbandabui@users.noreply.github.com>
2024-01-31 18:40:10 +0700

    a

--- src/Congrat.js
+++ src/Congrat.js
@@ -8,8 +8,8 @@ const Congrat = () => {
             <Text style={styles.t}>Success!</Text>
             <View style={styles.imgcontainer}><Image source={require('../asset/img/suc.png')} /><Image style={{ marginTop: 20 }} source={require('../asset/img/tick.png')} /></View>
             <Text style={styles.context}>Your order will be delivery soon. Thank you for chosing our app</Text>
-            <TouchableOpacity style={styles.btnCon}><Text style={{ color: 'white', textAlign: 'center' }}>aaa</Text></TouchableOpacity>
-            <TouchableOpacity style={styles.btnCon1}><Text style={{ color: 'white', textAlign: 'center' }}>aaa</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.btnCon}><Text style={{ color: 'white', textAlign: 'center', fontWeight: '600', fontSize: 18 }}>Track your order</Text></TouchableOpacity>
+            <TouchableOpacity style={styles.btnCon1}><Text style={{ color: 'black', textAlign: 'center', fontWeight: '600', fontSize: 18 }}>Back to home</Text></TouchableOpacity>
 
         </View>
     )
@@ -24,14 +24,20 @@ const styles = StyleSheet.create({
         height: 60,
         backgroundColor: "#242424",
         borderRadius: 15,
+        justifyContent: 'center',    // Center the text vertically
+        alignItems: 'center',
+
     },
     btnCon1: {
         marginTop: 40,
         width: 315,
         height: 60,
-        backgroundColor: "#242424",
+        backgroundColor: "white",   // Set the background color to white
+        borderColor: "#242424",     // Set the border color to black
+        borderWidth: 2,             // Set the border width
         borderRadius: 15,
-
+        justifyContent: 'center',    // Center the text vertically
+        alignItems: 'center',        // Center the text horizontally
     },
 
     container: {
