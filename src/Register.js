607cfa2833b785fd9506991a975aa47d212f34fa
ɭê ɦùռɠ <125887825+nguoilaisaochoi@users.noreply.github.com>
2024-02-18 15:54:14 +0700

    Revert "10/2/2024"
    This reverts commit 8bcc0d332f9cd7b4add89ee4c36a3f9516197fd8.

--- src/Register.js
+++ src/Register.js
@@ -5,28 +5,13 @@ import {
   View,
   TouchableOpacity,
   TextInput,
-  Alert,
 } from 'react-native';
-import React, {useContext, useState} from 'react';
-import {Appcontext} from './Appcontext';
-import MyOrder from './MyOrder';
+import React from 'react';
 
 const Register = props => {
   const {navigation} = props;
   const login = () => {
     navigation.navigate('Login');
-  };
-  const {setIsLogin} = useContext(Appcontext);
-  const [Name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const handleOnSumit = () => {
-    if (!Name || (Name.length < 1 && !password) || password.length < 1) {
-      Alert.alert('Information cannot be left blank!');
-      return;
-    } else {
-      setIsLogin(true);
-    }
-    return;
   };
   return (
     <View style={styles.container}>
@@ -38,7 +23,7 @@ const Register = props => {
         <View>
           <Image
             style={styles.logoac}
-            source={require('../asset/img/gr.png')}
+            source={require('../asset/img/logoac.png')}
           />
         </View>
 
@@ -50,49 +35,48 @@ const Register = props => {
       <View style={styles.welcome}>
         <Text style={styles.welcomeback}>WELCOME</Text>
       </View>
-      <View style={styles.styleinput}>
-        <Text style={styles.txtemail}>Name</Text>
+      <View style={styles.containerte}>
         <View style={styles.TextInput}>
-          <TextInput style={styles.text} onChange={text => setName(text)} />
+          <TextInput style={styles.text} placeholder="  Name" />
+          <TextInput
+            style={[styles.text, styles.textmail]}
+            placeholder="  Email"
+          />
         </View>
-        <Text style={styles.txtemail}>Email</Text>
-        <View style={styles.TextInput}>
-          <TextInput style={styles.text} onChange={text => setName(text)} />
-        </View>
-        <Text style={[styles.txtemail]}>Password</Text>
         <View style={styles.TextInput}>
           <TextInput
             style={styles.text}
+            placeholder=" Password"
             secureTextEntry={true}
-            onChange={text => setPassword(text)}
           />
           <Image
             style={styles.iconeye}
             source={require('../asset/img/eye2.png')}
           />
         </View>
-        <Text style={[styles.txtemail]}>Confirm Password</Text>
         <View style={styles.TextInput}>
           <TextInput
             style={styles.text}
+            placeholder=" Confirm Passwword"
             secureTextEntry={true}
-            onChange={text => setPassword(text)}
           />
           <Image
             style={styles.iconeye}
             source={require('../asset/img/eye2.png')}
           />
         </View>
-        <View style={styles.forgot}>
-          <TouchableOpacity
-            style={[styles.button, {marginRight: 17}]}
-            onPress={handleOnSumit}>
-            <Text style={[styles.textlg]}>SIGN UP</Text>
-          </TouchableOpacity>
-        </View>
-        <TouchableOpacity style={styles.sign} onPress={login}>
-          <Text>Already have account?</Text>
-          <Text style={{color: '#303030', fontWeight: 'bold'}}> SIGN IN</Text>
+      </View>
+
+      <View style={styles.forgot}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textlg}>SIGN UP</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.forgot}>
+        <TouchableOpacity onPress={login}>
+          <Text style={styles.textSU1}>
+            Already have account?<Text style={styles.textSU}> SIGN UP</Text>
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -101,128 +85,94 @@ const Register = props => {
 export default Register;
 
 const styles = StyleSheet.create({
-  sign: {
-    flexDirection:'row',
-    alignSelf:'center',
-    marginTop:'5%'
+  textSU1: {
+    fontSize: 17,
   },
-  txtemail: {
-    fontFamily: 'NunitoSans12pt-Regular',
-    marginLeft: 10,
-    color: '#909090',
+  textmail: {
+    marginTop: 15,
   },
-  styleinput: {
-    backgroundColor: '#FFFFFF',
-    width: '92%',
-    height: '66%',
-    elevation: 8,
-    paddingLeft: 17,
-    paddingTop: 17,
-    paddingBottom: 17,
-    marginTop: -20,
+  containerte: {
+    marginTop: 10,
   },
   textSU: {
     color: '#303030',
-    alignSelf: 'center',
+    textAlign: 'center',
     fontSize: 18,
-    marginRight: 17,
     fontStyle: 'normal',
     fontWeight: '600',
-    fontFamily: 'NunitoSans12pt-SemiBold',
   },
   textlg: {
-    alignSelf: 'center',
+    textAlign: 'center',
     color: '#fff',
     fontSize: 18,
     fontWeight: '600',
-    marginTop: 12,
+    marginTop: 22,
   },
   button: {
     borderRadius: 8,
-    width: '90%',
-    height: 50,
+    width: '100%',
+    height: 70,
     backgroundColor: '#242424',
     flexShrink: 0,
-    alignSelf: 'center',
   },
-  textfg: {
-    color: '#303030',
-    alignSelf: 'center',
-    fontSize: 18,
-    fontWeight: '600',
-    fontStyle: 'normal',
-    marginRight: 17,
-    fontFamily: 'NunitoSans12pt-SemiBold',
-  },
-  forgot: {
-    width: '90%',
 
-    alignSelf: 'center',
-    marginTop: 35,
+  forgot: {
+    alignItems: 'center',
+    marginTop: 55,
   },
   iconeye: {
     position: 'absolute',
-    top: 15,
+    top: 25,
     right: 20,
     backgroundColor: '#fff',
   },
   text: {
     width: '100%',
-    height: 48,
-    borderBottomWidth: 1,
-    borderColor: '#E0E0E0',
+    height: 60,
+    borderWidth: 1,
+    borderColor: '#252a32',
     borderRadius: 8,
     backgroundColor: '#fff',
     paddingVertical: 11,
     paddingHorizontal: 17,
   },
   TextInput: {
-    marginTop: 5,
+    marginTop: 15,
     width: '100%',
-    marginBottom: 10,
   },
   welcomeback: {
     color: '#303030',
     fontSize: 24,
+    fontStyle: 'normal',
+    fontWeight: '700',
     lineHeight: 45,
     letterSpacing: 1.2,
-    fontWeight: '700',
-    fontFamily: 'Merriweather-Bold',
-    textShadowColor: 'rgba(0, 0, 0, 0.3)',
-    textShadowOffset: {width: 1, height: 2},
-    textShadowRadius: 5,
-  },
-  hello: {
-    color: '#909090',
-    fontSize: 30,
-    fontStyle: 'normal',
-    fontWeight: '400',
-    lineHeight: 45,
-    fontFamily: 'Merriweather-Regular',
-    textShadowColor: 'rgba(0, 0, 0, 0.3)',
-    textShadowOffset: {width: 1, height: 2},
-    textShadowRadius: 5,
   },
   welcome: {
     left: 30,
-    marginTop: 15,
+    marginTop: 20,
     width: 216,
-    height: 93,
+    height: 40,
   },
   rectangri: {
     top: 53,
-    right: 35,
+    right: 20,
   },
   rectangle: {
     top: 53,
-    left: 30,
+    left: 20,
   },
   logoac: {
+    overflow: 'hidden',
     marginTop: 22,
-    resizeMode: 'contain',
-    width: 74,
-    height: 74,
+    justifyContent: 'space-between',
+    resizeMode: 'cover',
+    borderWidth: 6,
+    backgroundColor: 'black',
+    width: 64,
+    height: 64,
     flexShrink: 0,
+    borderRadius: 50,
   },
   row: {
     justifyContent: 'space-between',
@@ -233,6 +183,6 @@ const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: '100%',
-    backgroundColor: '#FFF',
+    padding: 17,
   },
 });
